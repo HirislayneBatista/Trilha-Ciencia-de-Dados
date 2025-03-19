69d43b9cdb8130bf242f5daf0cbd8e76cbbad27c
Hirislayne Batista <hirislaynebatista5@gmail.com>
2025-03-19 18:16:38 -0300

    Delete arquivo

--- Desafio_2/Questao_7.js
+++
@@ -1,8 +0,0 @@
-const readline = require('readline-sync'); 
-
-function calcularQuadrado(numero) {
-    return Math.pow(numero, 2)
-}
-
-let numero = Number(readline.question('\nInforme um numero para obter o seu quadrado: '));
-console.log(`O quadrado de ${numero} é ${calcularQuadrado(numero)}.\n`);
