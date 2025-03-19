4d9f4e1299d521aebc6e497dce202a7fee0d3d7f
Hirislayne Batista <hirislaynebatista5@gmail.com>
2025-03-19 18:16:26 -0300

    Delete arquivo

--- Desafio_2/Questao_6.js
+++
@@ -1,6 +0,0 @@
-const readline = require('readline-sync'); 
-
-function mensagem(nome){
-    return `\nOlá, ${nome}, que bom ter voce no programa Trilhas!\n`;
-}
-console.log(mensagem("Ciclano Jhonson"));
