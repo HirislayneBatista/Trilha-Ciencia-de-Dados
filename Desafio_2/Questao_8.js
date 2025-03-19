f907ca8f05cc8b2250c589b2eec82827141bd2c9
Hirislayne Batista <hirislaynebatista5@gmail.com>
2025-03-19 18:16:48 -0300

    Delete arquivo

--- Desafio_2/Questao_8.js
+++
@@ -1,7 +0,0 @@
-function subtracao(x, y) {
-    return x - y;
-}
-const numX = 10
-const numY = 20
-const resultado = subtracao(numX, numY)
-console.log(`O resultado da subtracao é: ${resultado}.\n`);
