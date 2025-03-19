e33cd51a47510a4ccdb411cf5660487b8f327347
Hirislayne Batista <hirislaynebatista5@gmail.com>
2025-03-19 18:16:00 -0300

    Delete arquivo

--- Desafio_2/Questao_4.js
+++
@@ -1,9 +0,0 @@
-const readline = require('readline-sync'); 
-
-let lista2 = [];
-console.log('Informe 5 numeros.');
-for (let i = 0; i < 5; i++){
-    let num = Number(readline.question(`Numero ${i + 1}: `));
-    lista2.push(num);
-}
-console.log("Os numeros informados foram:", lista2);
